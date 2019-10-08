feba620fa4304c19a5319810855c00950cbfb3ff
Unknown <jorgen.bengtsson@sigma.se>
2019-10-08 13:40:32 +0200

    Adding BackEnd End-points

---
+++ BackEnd/FilmenBackEnd/Controllers/viewingsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FilmenBackEnd.Models;
+using FilmenBackEnd.MovieDB;
+
+namespace FilmenBackEnd.Controllers
+{
+    public class ViewingsController : ApiController
+    {
+        private FilmenEntities db = new FilmenEntities();
+
+        // GET: api/viewings
+        public List<ViewingViewModel> GetViewings()
+        {
+            var listOfViewings = db.viewings.Include(v => v.lounge).OrderBy(v => v.date);
+            var listOfMovies = new List<ViewingViewModel>();
+
+            foreach(var viewing in listOfViewings)
+            {
+                 var movie = MovieFetcher.GetMovie(viewing.moviedbid);
+
+                listOfMovies.Add(new ViewingViewModel
+                {
+                    MovieName = movie.Title,
+                    Length = movie.Runtime ?? 0,
+                    Adult = movie.Adult,
+                    LoungeName = viewing.lounge.name,
+                    ViewingDate = viewing.date,
+                    TotalSeats = viewing.lounge.seats.Count()
+                });
+            }
+            
+            return listOfMovies;
+        }
+
+        // GET: api/viewings/5
+        [ResponseType(typeof(viewing))]
+        public IHttpActionResult Getviewing(int id)
+        {
+            viewing viewing = db.viewings.Find(id);
+            if (viewing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(viewing);
+        }
+
+        // PUT: api/viewings/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putviewing(int id, viewing viewing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != viewing.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(viewing).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!viewingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/viewings
+        [ResponseType(typeof(viewing))]
+        public IHttpActionResult Postviewing(viewing viewing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.viewings.Add(viewing);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = viewing.id }, viewing);
+        }
+
+        // DELETE: api/viewings/5
+        [ResponseType(typeof(viewing))]
+        public IHttpActionResult Deleteviewing(int id)
+        {
+            viewing viewing = db.viewings.Find(id);
+            if (viewing == null)
+            {
+                return NotFound();
+            }
+
+            db.viewings.Remove(viewing);
+            db.SaveChanges();
+
+            return Ok(viewing);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool viewingExists(int id)
+        {
+            return db.viewings.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ BackEnd/FilmenBackEnd/MovieDB/MovieFetcher.cs
@@ -0,0 +1,44 @@
+﻿using FilmenBackEnd.MovieDB.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+
+namespace FilmenBackEnd.MovieDB
+{
+    public static class MovieFetcher
+    {
+        public static MovieDBMovieInformation GetMovie(string id)
+        {
+            var apiKey = ConfigurationManager.AppSettings["MovieDBAPIKey"];
+            var movieEndPoint = ConfigurationManager.AppSettings["MovieDBMovieEndPoint"];
+            
+            string apiPath = String.Format(movieEndPoint, id, apiKey);
+
+            HttpWebRequest apiRequest = WebRequest.Create(apiPath) as HttpWebRequest;
+
+            var apiResponse = String.Empty;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3
+                        | SecurityProtocolType.Tls
+                        | SecurityProtocolType.Tls11
+                        | SecurityProtocolType.Tls12;
+            try
+            {
+                using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    apiResponse = reader.ReadToEnd();
+                }
+
+                MovieDBMovieInformation returnMovie = JsonConvert.DeserializeObject<MovieDBMovieInformation>(apiResponse);
+
+                return returnMovie;
+
+            } catch (System.Net.WebException ex)
+            {
+                throw new MovieDBFetchError("Error Fetching Movie info from MovieDB", ex.InnerException);
+            }
+        }
+    }
+}
